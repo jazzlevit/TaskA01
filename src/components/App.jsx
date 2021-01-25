9c2ec1b3e2665401e950a5dbfb2f4d1e659e86bb
ArturBelenchenko <you@example.com>
2021-01-25 20:37:44 +0200

    add div header.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
 import './Inner';
 import Inner from './Inner';
+import Header from './Header';
 
 
 class App extends Component {
-   render() {
-       return (
-           <div className="container">
-               <Inner />
-           </div>
+    render() {
+        return (
+            <div className="container">
+                <Header />
+                <Inner />
+            </div>
        )
-   }
+    }
 }
 
 
