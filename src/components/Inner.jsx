a1f7ef3e22b0a0da70a9c3a2b4a251fd73834190
ArturBelenchenko <you@example.com>
2021-01-25 19:13:49 +0200

    modified data and imagegit

--- src/components/Inner.jsx
+++ src/components/Inner.jsx
@@ -6,43 +6,43 @@ class Inner extends React.Component {
             <div className="inner">
                 <div id="1" className="inner__item">
                     <div className="image-wrapper">
-                        <img className="image" src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/5347c131056459.563f7852f0ba9.jpg" alt="pic" />
+                        <img className="image" src="https://i.pinimg.com/474x/01/73/f1/0173f1060ecafc83e58be838586517d5.jpg" alt="pic" />
                     </div>
                     <div className="title">Title</div>
-                    <div className="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo cumque eos cum natus autem. Sequi quibusdam dicta dolorum tenetur necessitatibus.</div>
-                    <button className="add-btn">lorem</button>
+                    <div className="description">$54000</div>
+                    <button className="add-btn">купить</button>
                 </div>
                 <div className="inner__item">
                     <div className="image-wrapper">
-                        <img className="image" src="https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/27df3895034859.5e8dbad5aa84e.jpg" alt="pic" />
+                        <img className="image" src="https://i.pinimg.com/474x/50/eb/d1/50ebd1ba0836937fd4da4489ebfaeb96.jpg" alt="pic" />
                     </div>
                     <div className="title">Title</div>
-                    <div className="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo cumque eos cum natus autem. Sequi quibusdam dicta dolorum tenetur necessitatibus.</div>
-                    <button className="add-btn">lorem</button>
+                    <div className="description">$48000</div>
+                    <button className="add-btn">купить</button>
                 </div>
                 <div className="inner__item">
                     <div className="image-wrapper">
-                        <img className="image" src="https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/e1ed2a55538223.5e3c4e15c2468.jpg" alt="pic" />
+                        <img className="image" src="https://i.pinimg.com/474x/87/55/3f/87553f94c3d3c608ab0a860175e53d92.jpg" alt="pic" />
                     </div>
                     <div className="title">Title</div>
-                    <div className="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo cumque eos cum natus autem. Sequi quibusdam dicta dolorum tenetur necessitatibus.</div>
-                    <button className="add-btn">lorem</button>
+                    <div className="description">$52000</div>
+                    <button className="add-btn">купить</button>
                 </div>
                 <div className="inner__item">
                     <div className="image-wrapper">
-                        <img className="image" src="https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/0e611315583921.5b74a2d613fa1.jpg" alt="pic" />
+                        <img className="image" src="https://i.pinimg.com/474x/c3/0e/a0/c30ea0cd5b016d32139a6642affe1ee9.jpg" alt="pic" />
                     </div>
                     <div className="title">Title</div>
-                    <div className="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo cumque eos cum natus autem. Sequi quibusdam dicta dolorum tenetur necessitatibus.</div>
-                    <button className="add-btn">lorem</button>
+                    <div className="description">$37000</div>
+                    <button className="add-btn">купить</button>
                 </div>
                 <div className="inner__item">
                     <div className="image-wrapper">
-                        <img className="image" src="https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/b7079b81426255.5cff75781fb03.jpg" alt="pic" />
+                        <img className="image" src="https://i.pinimg.com/474x/1b/2d/bc/1b2dbc99bb3e887b425dcae26b365d77.jpg" alt="pic" />
                     </div>
-                    <div className="title">Title</div>
-                    <div className="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo cumque eos cum natus autem. Sequi quibusdam dicta dolorum tenetur necessitatibus.</div>
-                    <button className="add-btn">lorem</button>
+                    <div className="title">Mercedes AMG GT63</div>
+                    <div className="description">$40000</div>
+                    <button className="add-btn">купить</button>
                 </div>
             </div>
         )
